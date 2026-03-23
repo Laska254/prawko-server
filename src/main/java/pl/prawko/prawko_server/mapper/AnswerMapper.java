1a33c55312a4b408379be0c6c64e964e6a3caf6c
Laska254 <w.turczak254@gmail.com>
2026-03-23 21:27:45 +0100

    doc

--- src/main/java/pl/prawko/prawko_server/mapper/AnswerMapper.java
+++ src/main/java/pl/prawko/prawko_server/mapper/AnswerMapper.java
@@ -8,10 +8,9 @@ import pl.prawko.prawko_server.dto.AnswerDto;
 import pl.prawko.prawko_server.dto.AnswerTranslationDto;
 import pl.prawko.prawko_server.model.Answer;
 import pl.prawko.prawko_server.model.AnswerTranslation;
-import pl.prawko.prawko_server.model.QuestionCSV;
 
 /**
- * This class is responsible for mapping {@link QuestionCSV} model into {@link Answer} entity.
+ * This class is responsible for mapping {@link Answer} related entities and dto's.
  * <p>
  * The mapper is registered as a Spring {@link Component}, so it can be injected into services or other components that require answer mapping
  * functionality.
