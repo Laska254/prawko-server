01f6f0cab66c40a6654858fbcce1b0c8598de5b5
Laska254 <w.turczak254@gmail.com>
2026-03-24 11:58:40 +0100

    Rebuild of tests for QuestionMapper

--- src/test/java/pl/prawko/prawko_server/mapper/QuestionMapperTest.java
+++ src/test/java/pl/prawko/prawko_server/mapper/QuestionMapperTest.java
@@ -1,52 +1,89 @@
 package pl.prawko.prawko_server.mapper;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.prawko.prawko_server.model.Language;
+import pl.prawko.prawko_server.model.QuestionType;
+import pl.prawko.prawko_server.test_data.QuestionCSVTestData;
+import pl.prawko.prawko_server.test_data.QuestionTestData;
+import pl.prawko.prawko_server.test_data.QuestionTranslationsTestData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class QuestionMapperTest {
 
-//    @Mock
-//    private CategoryService categoryService;
-//
-//    @Mock
-//    private LanguageService languageService;
-//
-//    @InjectMocks
-//    private QuestionMapper questionMapper;
-//
-//    private final List<Language> languages = LanguageTestData.ALL;
-//
-//    @BeforeEach
-//    void setUp() {
-//        final var answerMapper = new AnswerMapper(languageService);
-//        questionMapper = new QuestionMapper(categoryService, languageService, answerMapper);
-//        when(languageService.findAll()).thenReturn(languages);
-//    }
-//
-//        @Test
-//    void mapQuestionCSVToQuestion_returnBasicQuestion() {
-//        final var categories = List.of(CategoryTestData.CATEGORY_A, CategoryTestData.CATEGORY_B);
-//        when(categoryService.findAllFromString("A,B")).thenReturn(categories);
-//
-//        final var given = QuestionCSVTestData.createBasicQuestionCSV();
-//        final var expected = QuestionTestData.createQuestion(QuestionType.BASIC);
-//
-//        final var result = parser.mapQuestionCSVToQuestion(given);
-//
-//        assertThat(result).isEqualTo(expected);
-//    }
-//
-//    @Test
-//    void mapQuestionCSVToQuestion_returnSpecialQuestion() {
-//        final var category = CategoryTestData.CATEGORY_PT;
-//        when(categoryService.findAllFromString("PT")).thenReturn(List.of(category));
-//        final var given = QuestionCSVTestData.createSpecialQuestionCSV();
-//        final var expected = QuestionTestData.createQuestion(QuestionType.SPECIAL);
-//
-//        final var result = parser.mapQuestionCSVToQuestion(given);
-//
-//        assertThat(result).isEqualTo(expected);
-//    }
+    @Mock
+    private AnswerMapper answerMapper;
+
+    @InjectMocks
+    private QuestionMapperImpl questionMapper;
+
+    @Nested
+    class ToDto {
+
+        @ParameterizedTest
+        @EnumSource(value = QuestionType.class)
+        void correctlyMapBothQuestionTypes(QuestionType type) {
+            final var given = QuestionTestData.createQuestion(type);
+            final var expected = QuestionTestData.createQuestionDto(type);
+
+            final var result = questionMapper.toDto(given);
+
+            assertThat(result)
+                    .usingRecursiveComparison()
+                    .ignoringFields("answers")
+                    .isEqualTo(expected);
+            verify(answerMapper, times(given.getAnswers().size())).toDto(any());
+        }
+
+    }
+
+    @Nested
+    class ToTranslationDto {
+
+        @ParameterizedTest
+        @MethodSource("pl.prawko.prawko_server.test_data.QuestionTranslationsTestData#translations")
+        void correctlyMapsTranslation(Language language, String content) {
+            final var given = QuestionTranslationsTestData.createTranslation(language, content);
+
+            final var result = questionMapper.toTranslationDto(given);
+
+            assertThat(result.languageCode()).isEqualTo(language.getCode());
+            assertThat(result.content()).isEqualTo(content);
+        }
+
+    }
+
+    @Nested
+    class ToEntity {
+
+        @ParameterizedTest
+        @EnumSource(value = QuestionType.class)
+        void correctlyMaps(QuestionType type) {
+            final var given = QuestionCSVTestData.createQuestionCSV(type);
+            final var expected = QuestionTestData.createQuestion(type);
+
+            final var result = questionMapper.toEntity(given);
+
+            assertThat(result)
+                    .usingRecursiveComparison()
+                    .ignoringFields("translations", "answers", "categories", "exams")
+                    .isEqualTo(expected);
+            assertThat(result.getTranslations()).isNull();
+            assertThat(result.getAnswers()).isNull();
+            assertThat(result.getCategories()).isNull();
+        }
+
+    }
 
 }
